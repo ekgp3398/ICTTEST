246a1833efa2a8c3501120b1f0b6db5bc3def5ba
dahye <ekgp3398@naver.com>
2022-11-15 21:24:51 +0900

    20221115

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,53 +1,52 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCopy, faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import './Home.css'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCopy,
+  faArrowAltCircleRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import "./Home.css";
 
 const Home = () => {
-    const navigate = useNavigate();
-    const url = window.location.href; // url 복사
-    const copyAlert = () => {
-        alert('링크 생성!');
-    };
+  const navigate = useNavigate();
+  const url = window.location.href; // url 복사
+  const copyAlert = () => {
+    alert("링크 생성!");
+  };
 
-    const Start = () => {
-        navigate('/Question')
-    }
-    return(
-        <div className= "wrapper">
-                <div className= "container">
-                    
-                    <div className= "middle">
-                        <h2 className= "header">나에게 맞는 동아리는?</h2>
-                        <p>총 7개의 질문을 통해</p>
-                        <p>나에게 맞는 동아리들을 추천해드려요.</p>
-                    </div>
-                    <div className= "btn_box">
-                        <button className = "start__button" onClick = {() => Start()}>
-                        시작하기
-                            <FontAwesomeIcon
-                                icon={faArrowAltCircleRight}
-                                className= "icon"
-                            />
-                        </button>
-                        <CopyToClipboard text={url}>
-                            <button
-                                className= "start__button"
-                                onClick={copyAlert}
-                            >
-                                링크복사
-                                <FontAwesomeIcon
-                                    icon={faCopy}
-                                    className= "icon"
-                                />
-                            </button>
-                        </CopyToClipboard>
-                    </div>
-                </div>
-            </div>
-    )
-}
+  const Start = () => {
+    navigate("/Question");
+  };
+  return (
+    <div className="background_wrapper">
+      <div className="wrapper">
+        <div className="container">
+          <div className="middle">
+            <h2 className="header">나에게 맞는 동아리는?</h2>
+            <img
+              className="club_icon"
+              src="https://cdn-icons-png.flaticon.com/512/8310/8310173.png"
+            />
+            <p>총 7개의 질문을 통해</p>
+            <p>나에게 맞는 동아리들을 추천해드려요.</p>
+          </div>
+          <div className="btn_box">
+            <button className="start__button" onClick={() => Start()}>
+              시작하기
+              <FontAwesomeIcon icon={faArrowAltCircleRight} className="icon" />
+            </button>
+            <CopyToClipboard text={url}>
+              <button className="start__button" onClick={copyAlert}>
+                링크복사
+                <FontAwesomeIcon icon={faCopy} className="icon" />
+              </button>
+            </CopyToClipboard>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -1,81 +1,92 @@
-import React, { useEffect, useState } from 'react';
-import './Option.css';
-import { useNavigate } from 'react-router-dom';
-import Questions from '../../common/api/Question.json';
+import React, { useEffect, useState } from "react";
+import "./Option.css";
+import { useNavigate } from "react-router-dom";
+import LoadingSpin from "react-loading-spin";
+import Questions from "../../common/api/Question.json";
 
 const Option = () => {
-    const [num, setNum] = useState(0);
-    const [currentSlide, setCurrentSlide] = useState(1);
-    const TOTAL_SLIDES = 7;
-    const navigate = useNavigate();
-    const [club, setClub] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [num, setNum] = useState(0);
+  const [currentSlide, setCurrentSlide] = useState(1);
+  const TOTAL_SLIDES = 7;
+  const navigate = useNavigate();
+  const [club, setClub] = useState([]);
 
-    const Answer = (props) => {
-        setClub(club + Questions[num].answers[props].type);
-        setNum(num + 1);
-        setCurrentSlide(currentSlide + 1);
-        console.log(club);
-    };
+  const Answer = (props) => {
+    setClub(club + Questions[num].answers[props].type);
+    setNum(num + 1);
+    setCurrentSlide(currentSlide + 1);
+    console.log(club);
+  };
 
-    const clubChecker = () => {
-        let map = {};
-        let result;
-        for(let i = 0; i < club.length; i++){
-            if(club[i] in map){
-                map[club[i]] += 1;
-            } else {
-                map[club[i]] = 1;
-            }
-        }
-        for(let count in map) {
-            if (map[count] > 2) {
-                result = count;
-            }
-        }
-        setTimeout(() => {
-            navigate(`/result/${result}`);
-        }, 3000);
+  const clubChecker = () => {
+    setLoading(true);
+    let map = {};
+    let result;
+    for (let i = 0; i < club.length; i++) {
+      if (club[i] in map) {
+        map[club[i]] += 1;
+      } else {
+        map[club[i]] = 1;
+      }
     }
-    useEffect(() => {
-        currentSlide > TOTAL_SLIDES && clubChecker();
-    }, [currentSlide]);
+    for (let count in map) {
+      if (map[count] > 2) {
+        result = count;
+      }
+    }
+    setTimeout(() => {
+      navigate(`/result/${result}`);
+    }, 3000);
+  };
+  useEffect(() => {
+    currentSlide > TOTAL_SLIDES && clubChecker();
+  }, [currentSlide]);
 
-    return (
-        <div className = "body_container">
-            <section className='container'>
-                <div className = "slider">
-                    {Questions.filter((item) => item.id === currentSlide).map((item, index) => (
-                        <div className = "content" key = {index}>
-                            <div className = "top">
-                                <div className = "counter">
-                                    <span className = "current_progress">
-                                        {currentSlide}
-                                    </span>
-                                    <span className = "end_progress">
-                                        /{TOTAL_SLIDES}
-                                    </span>
-                                </div>
-                                <h1 className = "club_question">
-                                    {item.question}
-                                </h1>
-                            </div>
-                            <article className = "answer_container">
-                                <button className = "answer_btn" onClick = {() => Answer(0)}>
-                                    {item.answers[0].content }
-                                </button>
-                                <button className = "answer_btn" onClick = {() => Answer(1)}>
-                                    {item.answers[1].content}
-                                </button>
-                                <button className = "answer_btn" onClick = {() => Answer(2)}>
-                                    {item.answers[2].content}
-                                </button>
-                            </article>
-                        </div>
-                    ))}
-                </div>
-            </section>
+  return (
+    <div className="background_wrapper">
+      {!loading ? (
+        <div className="body_container">
+          <section className="container">
+            <div className="slider">
+              {Questions.filter((item) => item.id === currentSlide).map(
+                (item, index) => (
+                  <div className="content" key={index}>
+                    <div className="top">
+                      <div className="counter">
+                        <span className="current_progress">{currentSlide}</span>
+                        <span className="end_progress">/{TOTAL_SLIDES}</span>
+                      </div>
+                      <h1 className="club_question">{item.question}</h1>
+                    </div>
+                    <article className="answer_container">
+                      <button className="answer_btn" onClick={() => Answer(0)}>
+                        {item.answers[0].content}
+                      </button>
+                      <button className="answer_btn" onClick={() => Answer(1)}>
+                        {item.answers[1].content}
+                      </button>
+                      <button className="answer_btn" onClick={() => Answer(2)}>
+                        {item.answers[2].content}
+                      </button>
+                    </article>
+                  </div>
+                )
+              )}
+            </div>
+          </section>
         </div>
-    );
+      ) : (
+        <div className="body_container">
+          <div className="container">
+            <div className="loading__container">
+              <LoadingSpin primaryColor="#ed6174" />
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Option;
